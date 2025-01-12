42c783a277f857ae2e3683b5203fbc28e842203f
Tushar Personal <tusharpersonal@Admins-MacBook-Pro.local>
2025-01-12 18:17:20 +0530

    feat: add readme and app filte

---
+++ App.js
@@ -0,0 +1,13 @@
+const parentDiv = React.createElement("div", {id: 'parent'}, [
+    React.createElement('div', {id: "child1"}, [
+       React.createElement('h1', {}, "Heading 1"),
+       React.createElement('h1', {}, "Heading 1")
+    ]),
+    React.createElement('div', {id: "child2"}, [
+       React.createElement('h1', {}, "Heading 2"),
+       React.createElement('h1', {}, "Heading 2")
+    ])
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parentDiv)
